6a2e58b3522708525303fd125afadfe963271746
JlSanchez05 <luisdevops0@gmail.com>
2022-08-23 15:22:29 -0400

    app is complete very basic

---
+++ script.js
@@ -0,0 +1,38 @@
+
+let heading = document.getElementById('quote')
+
+
+
+
+const apiUrl = 'https://favqs.com/api/qotd'
+
+fetch(apiUrl).then((response)=>{
+    if(response.ok){
+        return response.json()
+    }else{
+        throw new Error ('NetWork response error')
+    }
+}).then(results =>{
+    console.log(results)
+    displayUser(results)
+}).catch((error)=>console.error('fetch error',error))
+
+
+
+function displayUser(results){
+
+ let body = results.quote
+ heading.textContent= body.body
+console.log(body.body)
+
+}
+window.setInterval('refresh()', 5000); 	
+// Call a function every 10000 milliseconds 
+// (OR 10 seconds).
+
+// Refresh or reload page.
+function refresh() {
+    window .location.reload();
+}
+
+
